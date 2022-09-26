4214c75c7bb0daa53833f5b4faf8ef238fa02d84
mochi182 <mochi182tv@gmail.com>
2022-09-26 15:47:21 -0500

    + recursos y ruta

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-$(":checkbox").change(function() {
+function color_checkbox(){
     if(this.checked) {
         $(this).closest('tr').addClass("selected");  
     } else {
         $(this).closest('tr').removeClass("selected");  
     }
-});
+}
 
+$(":checkbox").change(color_checkbox);
+
+$( window ).on( "load", function(){
+    $(":checkbox").trigger( "change" );
+});
